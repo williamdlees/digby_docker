d26d5ae14307f763d997c4aa2a31eb2bf206985c
William Lees <william@lees.org.uk>
2020-11-10 15:16:07 +0000

    FIx standalone study_data

--- containers/my_flask/app/start.sh
+++ containers/my_flask/app/start.sh
@@ -6,6 +6,16 @@
 sleep 20
 
 cd /app
+
+if [ -f /study_data/.gitkeep ]; then
+  echo "initialising study_data"
+  rm /study_data/.gitkeep
+  cp -r static/init_study_data/* /study_data/.
+fi
+
+#while true; do sleep 60; done
+
+
 echo "migrating database"
 if [ ! -f migrations/README ]; then
   rm migrations/.gitkeep
@@ -24,3 +34,6 @@ celery -A app:celery worker -l INFO --logfile /config/log/celery.log&
 echo "starting gunicorn"
 export PYTHONUNBUFFERED=1
 exec gunicorn -b :5000 --timeout 300 --limit-request-line 0 --worker-class gthread --keep-alive 5 --workers=10 --graceful-timeout 900 --access-logfile /config/log/access.log --error-logfile /config/log/error.log --capture-output --log-level debug app:app 
+
+while true; do sleep 60; done
+
