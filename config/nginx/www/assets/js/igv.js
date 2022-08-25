af7940eb47ee7dab06b97cf13cb45bbd63ce4bd8
William Lees <william@lees.org.uk>
2022-08-25 17:08:44 +0100

    New client

--- config/nginx/www/assets/js/igv.js
+++ config/nginx/www/assets/js/igv.js
@@ -37806,7 +37806,7 @@
           const pos = readInt(ba, offset + 8);
 
           if (blockEnd > ba.length) {
-            console.log('finished block (no return value');
+            //console.log('finished block (no return value');
             return;
           }
 
@@ -37814,7 +37814,7 @@
             offset = blockEnd;
             continue; // unmapped read
           } else if (chrIdx !== undefined && (refID > chrIdx || pos > max)) {
-            console.log('decodeBamRecords: got to end');
+            //console.log('decodeBamRecords: got to end');
             return true; // off right edge, we're done
           } else if (chrIdx !== undefined && refID < chrIdx) {
             offset = blockEnd;
@@ -37838,7 +37838,7 @@
           }
 
           readName = readName.join('');
-          console.log('readName: ' + readName);
+          //console.log('readName: ' + readName);
           let lengthOnRef = 0;
           let cigar = '';
           let p = offset + 36 + nl;
@@ -38374,7 +38374,7 @@
       }
 
       async readAlignments(chr, bpStart, bpEnd) {
-        console.log('in bamreader.readalignments');
+        //console.log('in bamreader.readalignments');
         const chrToIndex = await this.getChrIndex();
         const queryChr = this.chrAliasTable.hasOwnProperty(chr) ? this.chrAliasTable[chr] : chr;
         const chrId = chrToIndex[queryChr];
@@ -38416,11 +38416,11 @@
               range: range
             }));
             var ba = unbgzf(compressed); //new Uint8Array(BGZip.unbgzf(compressed)); //, c.maxv.block - c.minv.block + 1));
-            console.log('loaded chunk');
+            //console.log('loaded chunk');
             const done = BamUtils.decodeBamRecords(ba, c.minv.offset, alignmentContainer, this.indexToChr, chrId, bpStart, bpEnd, this.filter);
 
             if (done) {
-              console.log('done loading chunks');
+            //  console.log('done loading chunks');
               // console.log(`Loaded ${counter} chunks out of  ${chunks.length}`);
               break;
             }
@@ -54711,12 +54711,12 @@
         const genome = this.genome;
         const showSoftClips = this.showSoftClips;
 
-        console.log('in getALignments');
+        //console.log('in getALignments');
         if (this.alignmentContainer && this.alignmentContainer.contains(chr, bpStart, bpEnd)) {
-          console.log('returning cached container');
+        //  console.log('returning cached container');
           return this.alignmentContainer;
         } else {
-          console.log('refreshing alignments');
+          //console.log('refreshing alignments');
           const alignmentContainer = await this.bamReader.readAlignments(chr, bpStart, bpEnd);
           let alignments = alignmentContainer.alignments;
 
@@ -54736,7 +54736,7 @@
             const sequence = await genome.sequence.getSequence(chr, alignmentContainer.start, alignmentContainer.end);
 
             if (sequence) {
-              console.log('got something back');
+              //console.log('got something back');
               alignmentContainer.coverageMap.refSeq = sequence; // TODO -- fix this
 
               alignmentContainer.sequence = sequence; // TODO -- fix this
@@ -54880,7 +54880,7 @@
       }
 
       async getFeatures(chr, bpStart, bpEnd, bpPerPixel, viewport) {
-        console.log('bamtrack.getFeatures: start ' + bpStart + 'end ' + bpEnd);
+        //console.log('bamtrack.getFeatures: start ' + bpStart + 'end ' + bpEnd);
         const alignmentContainer = await this.featureSource.getAlignments(chr, bpStart, bpEnd);
 
         if (alignmentContainer.alignments && alignmentContainer.alignments.length > 99) {
@@ -55628,7 +55628,7 @@
             const lastBlockReverseStrand = false === alignment.strand && b === 0;
             const lastBlock = lastBlockPositiveStrand | lastBlockReverseStrand;
 
-            console.log("drawBlock");
+            //console.log("drawBlock");
 
             if (lastBlock) {
               let xListPixel;
@@ -56254,7 +56254,7 @@
           visibilityWindow
         }); // Check for score or value
 
-        console.log('getFeatures: start $(start) end $(end)');
+        //console.log('getFeatures: start $(start) end $(end)');
 
         if (this.hasValue === undefined && features && features.length > 0) {
           this.hasValue = features[0].score !== undefined;
