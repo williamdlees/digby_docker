b39162f8355f94d5f678f6961a93bab644042788
William Lees <william@lees.org.uk>
2022-11-02 12:48:37 +0000

    Tunnig options for worker threads

--- containers/my_flask/app/start.sh
+++ containers/my_flask/app/start.sh
@@ -36,10 +36,10 @@ service rabbitmq-server restart
 echo "starting redis"
 service redis-server restart
 echo "starting celery workers"
-celery -A app:celery worker -l INFO --logfile /config/log/celery.log&
+celery -A app:celery worker -c $CELERY_WORKERS -l INFO --logfile /config/log/celery.log&
 echo "starting gunicorn"
 export PYTHONUNBUFFERED=1
-exec gunicorn -b :5000 --timeout 300 --limit-request-line 0 --worker-class gthread --keep-alive 5 --workers=5 --graceful-timeout 900 --access-logfile /config/log/access.log --error-logfile /config/log/error.log --capture-output --log-level debug app:app 
+exec gunicorn -b :5000 --timeout 300 --limit-request-line 0 --worker-class gthread --keep-alive 5 --workers=$GUNICORN_WORKERS --graceful-timeout 900 --access-logfile /config/log/access.log --error-logfile /config/log/error.log --capture-output --log-level debug app:app 
 
 while true; do sleep 60; done
 
